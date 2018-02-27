910e8ab90e016d440f8657f6d1e13617a6a054b7
nuh1234 <nuh.mohamud@gmail.com>
2018-02-27 15:17:47 -0800

    HTTP config

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
 namespace web {
     public class HelloController : Controller{
-        [HttpGetAtribute]
+
+        public static void Register(HttpConfiguration config) {
+        
+        }
         public string index() {
             return "Browser loaded";
         }
